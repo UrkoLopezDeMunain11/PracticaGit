5b3667cba8a9a13d7f214d4e5afe6710bfa0cee9
Urko <urko.lopezdemunain.44772@ikasle.egibide.org>
2025-10-22 11:48:52 +0200

    Rama main

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,52 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        static String cadena ="";
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int opcion = 0;
+            do {
+                try {
+                    System.out.println("-----------------------------------------------------------------------------");
+                    System.out.println("                            ----MENU----                                     ");
+                    System.out.println("1. Solicitar una cadena de caracteres");
+                    System.out.println("2. Visualizar el caracter de la posicion x de una cadena de caracteres");
+                    System.out.println("3. Convertir la cadena de caracteres en un numero entero");
+                    System.out.println("4. Convertir la cadena de caracteres en una fecha");
+                    System.out.println("5. Finalizar");
+                    System.out.println("-----------------------------------------------------------------------------");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+                    System.out.println("introduce una opcion: ");
+                    opcion = sc.nextInt();
+                    switch (opcion) {
+                        case 1:
+                            SolicitarCadenaCaracteres();
+                            break;
+                        case 2:
+                            VisualizarCaracter();
+                            break;
+                        case 3:
+                            ConvertirNumeroEntero();
+                            break;
+                        case 4:
+                            ConvertirFecha();
+                            break;
+                        case 5:
+                            System.out.println("programa finalizado");
+                            break;
+                        default:
+                            System.out.println("Opcion no permitida, introduzca una de las siguientes opciones");
+                            break;
+                    }
+                } catch (InputMismatchException e) {
+                    System.out.println("Opcion no permitida,introduzca una de las siguientes opciones");
+                    sc.nextLine();//paqra limpiar y te salgaq lño siguiente bien
+                }
+            }while(opcion !=5);
         }
     }
 }
